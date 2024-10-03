53c53d2f49f8d12c903c7ec76233a83e8fe1f806
raihanArman <raihanarman8@gmail.com>
2024-10-03 09:39:37 +0800

    fixing issues saver on android

--- composeApp/src/commonMain/kotlin/com/randev/kmmgmaps/maps/CameraCoordinate.kt
+++ composeApp/src/commonMain/kotlin/com/randev/kmmgmaps/maps/CameraCoordinate.kt
@@ -1,16 +1,19 @@
 package com.randev.kmmgmaps.maps
 
+import com.bumble.appyx.utils.multiplatform.Parcelable
+import com.bumble.appyx.utils.multiplatform.Parcelize
 import com.randev.kmmgmaps.network.data.Coordinate
 
 /**
  * @author Raihan Arman
  * @date 02/10/24
  */
+@Parcelize
 data class CameraCoordinate(
     val coordinate: Coordinate = Coordinate(),
     val zoom: Float? = null,
     val initializer: Int? = null
-) {
+): Parcelable {
     fun zoomWithDefault(): Float {
         return zoom ?: 16f
     }
--- composeApp/src/commonMain/kotlin/com/randev/kmmgmaps/network/data/Coordinate.kt
+++ composeApp/src/commonMain/kotlin/com/randev/kmmgmaps/network/data/Coordinate.kt
@@ -1,13 +1,17 @@
 package com.randev.kmmgmaps.network.data
 
+import com.bumble.appyx.utils.multiplatform.Parcelable
+import com.bumble.appyx.utils.multiplatform.Parcelize
+
 /**
  * @author Raihan Arman
  * @date 02/10/24
  */
+@Parcelize
 data class Coordinate(
     val latitude: Double = 0.0,
     val longitude: Double = 0.0
-) {
+): Parcelable {
     override fun toString(): String {
         return "$latitude,$longitude"
     }
